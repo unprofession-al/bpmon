bfe49aace7991e3a68c5168d2e5e6825d95d069c
Daniel Menet <membership@sontags.ch>
2017-12-27 17:34:37 +0100

    fixed tests

--- store_mock_test.go
+++ store_mock_test.go
@@ -3,6 +3,7 @@ package bpmon
 import (
 	"time"
 
+	"github.com/unprofession-al/bpmon/status"
 	"github.com/unprofession-al/bpmon/store"
 )
 
@@ -16,7 +17,7 @@ func (pp StoreMock) GetLatest(rs store.ResultSet) (store.ResultSet, error) {
 	return store.ResultSet{}, nil
 }
 
-func (pp StoreMock) GetEvents(rs store.ResultSet, start time.Time, end time.Time, interval time.Duration) ([]store.Event, error) {
+func (pp StoreMock) GetEvents(rs store.ResultSet, start time.Time, end time.Time, interval time.Duration, stati []status.Status) ([]store.Event, error) {
 	return []store.Event{}, nil
 }
 func (pp StoreMock) AnnotateEvent(id string, annotation string) (store.ResultSet, error) {
