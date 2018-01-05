256307b8390143e96a1e9286e232cd85700808c5
Daniel Menet <membership@sontags.ch>
2018-01-05 11:26:23 +0100

    added doc

--- checker/checker.go
+++ checker/checker.go
@@ -1,3 +1,7 @@
+// Package checker provides a generic interface to interact with service status
+// provider implementations such as the Icinga implementaton.
+//
+// The checker package must be used together with on implementation.
 package checker
 
 import (
@@ -13,17 +17,33 @@ var (
 	c   = make(map[string]func(Conf) (Checker, error))
 )
 
+// Conf keeps the configuration of the checker implementation. This struct is
+// passed to the store implementatinon itself via the registerd setup function.
+// The field 'Kind' is used to determine which provider is requested.
 type Conf struct {
 	Kind       string `yaml:"kind"`
 	Connection string `yaml:"connection"`
 }
 
+// Checker interface needs to be implemented in order to provide a Checker
+// backend such as Icinga.
 type Checker interface {
-	Status(string, string) Result
+	// Status takes a host string as well as a service string and returns
+	// 'Result' of the stuct of the check.
+	Status(host string, service string) Result
+
+	// Values returns a lists of value names that a 'Result' stuct will contain
+	// when 'Status()' is called.
 	Values() []string
+
+	// Each checked provides its own default rules on which a 'Result' status
+	// is evaluated in order to get a 'status.Status'.
 	DefaultRules() rules.Rules
 }
 
+// Register must be called in the init function of each checker implementation.
+// The Register function will panic if two checker impelmentations with the
+// same name try to register themselfs.
 func Register(name string, setupFunc func(Conf) (Checker, error)) {
 	cMu.Lock()
 	defer cMu.Unlock()
@@ -33,6 +53,9 @@ func Register(name string, setupFunc func(Conf) (Checker, error)) {
 	c[name] = setupFunc
 }
 
+// New well return a configured instance of a checker implementation. The
+// implementation requested is determined by the 'Kind' field of the
+// configuration struct.
 func New(conf Conf) (Checker, error) {
 	setupFunc, ok := c[conf.Kind]
 	if !ok {
@@ -41,6 +64,10 @@ func New(conf Conf) (Checker, error) {
 	return setupFunc(conf)
 }
 
+// Result is returned on a service status check. It contains all relevant
+// information in the effective result of the check in the 'Values' map.
+// If an error occures while performing the check, it is stored in the 'Error'
+// field.
 type Result struct {
 	Timestamp time.Time
 	Message   string
