2bc3c54ff34957ddcfeba10a53e94ffec235b90f
Daniel Menet <membership@sontags.ch>
2018-03-08 13:42:27 +0100

    finalized demo implementation

--- cmd/bpmon/annotate.go
+++ cmd/bpmon/annotate.go
@@ -4,12 +4,18 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/spf13/cobra"
 	"github.com/unprofession-al/bpmon"
 	"github.com/unprofession-al/bpmon/periphery/annotate"
 	"github.com/unprofession-al/bpmon/store"
 	_ "github.com/unprofession-al/bpmon/store/influx"
+)
+
+var (
+	annotateAuthHeaderName   string
+	annotateAuthHeaderValues string
 )
 
 var annotateCmd = &cobra.Command{
@@ -24,7 +30,12 @@ var annotateCmd = &cobra.Command{
 
 		pp, _ := store.New(c.Store)
 
-		router, err := annotate.Setup(c.Annotate, bp, pp)
+		headerValues := strings.Split(annotateAuthHeaderValues, ",")
+		for i, val := range headerValues {
+			headerValues[i] = strings.TrimSpace(val)
+		}
+
+		router, err := annotate.Setup(c.Annotate, bp, pp, annotateAuthHeaderName, headerValues)
 		if err != nil {
 			msg := fmt.Sprintf("Could not build router for server: %s", err.Error())
 			log.Fatal(msg)
@@ -37,4 +48,6 @@ var annotateCmd = &cobra.Command{
 
 func init() {
 	betaCmd.AddCommand(annotateCmd)
+	annotateCmd.PersistentFlags().StringVarP(&annotateAuthHeaderName, "auth-header-name", "", "", "HTTP header name to check")
+	annotateCmd.PersistentFlags().StringVarP(&annotateAuthHeaderValues, "auth-header-values", "", "", "HTTP header values to be allowed, comma separated list")
 }
--- periphery/webhelpers/middleware.go
+++ periphery/webhelpers/middleware.go
@@ -2,8 +2,11 @@ package webhelpers
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"strings"
+	"time"
 )
 
 type TokenAuth struct {
@@ -51,6 +54,92 @@ func RecipientsHeaderAuth(next http.Handler, RecipientsHeaderName string) http.H
 		}
 		ctx := context.WithValue(r.Context(), KeyRecipients, groups)
 		next.ServeHTTP(w, r.WithContext(ctx))
+	}
+
+	return http.HandlerFunc(fn)
+}
+
+func HeaderAuthMatcher(next http.Handler, HeaderName string, HeaderValuesAllowed []string) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		var values []string
+
+		headerContent := r.Header.Get(HeaderName)
+		if headerContent != "" {
+			values = strings.Split(headerContent, ",")
+		}
+
+		isAllowed := false
+
+		for _, val := range values {
+			for _, matchVal := range HeaderValuesAllowed {
+				if val == matchVal {
+					isAllowed = true
+				}
+			}
+		}
+
+		if !isAllowed {
+			http.Error(w, "authorization failed", http.StatusUnauthorized)
+			return
+		}
+		next.ServeHTTP(w, r)
+	}
+
+	return http.HandlerFunc(fn)
+}
+
+type logmgs struct {
+	Timestamp string        `json:"timestamp"`
+	Status    int           `json:"status"`
+	Size      int           `json:"size"`
+	Method    string        `json:"method"`
+	Request   string        `json:"request"`
+	Latency   time.Duration `json:"latency"`
+}
+
+type customResponseWriter struct {
+	http.ResponseWriter
+	status int
+	size   int
+}
+
+func (c *customResponseWriter) WriteHeader(status int) {
+	c.status = status
+	c.ResponseWriter.WriteHeader(status)
+}
+
+func (c *customResponseWriter) Write(b []byte) (int, error) {
+	size, err := c.ResponseWriter.Write(b)
+	c.size += size
+	return size, err
+}
+
+func newCustomResponseWriter(w http.ResponseWriter) *customResponseWriter {
+	// When WriteHeader is not called, it's safe to assume the status will be 200.
+	return &customResponseWriter{
+		ResponseWriter: w,
+		status:         200,
+	}
+}
+
+func Logger(next http.Handler) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		crw := newCustomResponseWriter(w)
+		next.ServeHTTP(crw, r)
+		end := time.Now()
+
+		msg := &logmgs{
+			Timestamp: end.Format("2006/01/02-15:04:05.000"),
+			Status:    crw.status,
+			Size:      crw.size,
+			Latency:   end.Sub(start),
+			Method:    r.Method,
+			Request:   r.URL.Path,
+		}
+
+		b, _ := json.Marshal(msg)
+		fmt.Println(string(b))
 	}
 
 	return http.HandlerFunc(fn)
