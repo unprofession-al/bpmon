6073607015a2ce0d0e2d7683ac065966fd4e20fd
Daniel Menet <daniel.menet@swisstopo.ch>
2020-01-07 13:47:21 +0100

    cli: fixed parameter issue

--- cmd/bpmon/cli.go
+++ cmd/bpmon/cli.go
@@ -140,7 +140,7 @@ func NewApp() *App {
 	return a
 }
 
-func (a App) configPrintCmd(cmd *cobra.Command, args []string) {
+func (a *App) configPrintCmd(cmd *cobra.Command, args []string) {
 	cfg := fmt.Sprintf("%s/%s", a.cfg.cfgBase, a.cfg.cfgFile)
 	c, _, err := config.NewFromFile(cfg, a.cfg.injectDefaults)
 	if err != nil {
@@ -168,12 +168,12 @@ func (a App) configPrintCmd(cmd *cobra.Command, args []string) {
 	fmt.Println(string(out))
 }
 
-func (a App) configInitCmd(cmd *cobra.Command, args []string) {
+func (a *App) configInitCmd(cmd *cobra.Command, args []string) {
 	out := config.ExampleYAML(a.cfg.configInitComments)
 	fmt.Println(string(out))
 }
 
-func (a App) configRawCmd(cmd *cobra.Command, args []string) {
+func (a *App) configRawCmd(cmd *cobra.Command, args []string) {
 	cfg := fmt.Sprintf("%s/%s", a.cfg.cfgBase, a.cfg.cfgFile)
 	_, raw, err := config.NewFromFile(cfg, a.cfg.injectDefaults)
 	if err != nil {
@@ -182,7 +182,7 @@ func (a App) configRawCmd(cmd *cobra.Command, args []string) {
 	fmt.Println(string(raw))
 }
 
-func (a App) dashboardCmd(cmd *cobra.Command, args []string) {
+func (a *App) dashboardCmd(cmd *cobra.Command, args []string) {
 	cfg := fmt.Sprintf("%s/%s", a.cfg.cfgBase, a.cfg.cfgFile)
 	c, _, err := config.NewFromFile(cfg, a.cfg.injectDefaults)
 	if err != nil {
@@ -217,7 +217,7 @@ func (a App) dashboardCmd(cmd *cobra.Command, args []string) {
 	d.Run()
 }
 
-func (a App) runCmd(cmd *cobra.Command, args []string) {
+func (a *App) runCmd(cmd *cobra.Command, args []string) {
 	runnerName := "default"
 	if len(args) > 0 {
 		runnerName = args[0]
@@ -346,7 +346,7 @@ func (a App) runCmd(cmd *cobra.Command, args []string) {
 	}
 }
 
-func (a App) writeCmd(cmd *cobra.Command, args []string) {
+func (a *App) writeCmd(cmd *cobra.Command, args []string) {
 	cfg := fmt.Sprintf("%s/%s", a.cfg.cfgBase, a.cfg.cfgFile)
 	c, _, err := config.NewFromFile(cfg, a.cfg.injectDefaults)
 	if err != nil {
@@ -381,6 +381,6 @@ func (a App) writeCmd(cmd *cobra.Command, args []string) {
 	}
 }
 
-func (a App) versionCmd(cmd *cobra.Command, args []string) {
+func (a *App) versionCmd(cmd *cobra.Command, args []string) {
 	fmt.Println(versionInfo())
 }
